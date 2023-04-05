491258fc8a7ead267c2e6c36950507456fd1ec3d
dgsglryz <dogusguleryuz@gmail.com>
2023-04-04 18:20:43 -0700

    bug fix

--- PlayChess/main.swift
+++ PlayChess/main.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
-let game = Game()
+let allPieces = [Pawn(isWhite: false, promoted: false, newPiece: nil),
+                 Knight(isWhite: false),
+                 Bishop(isWhite: false),
+                 Rook(isWhite: false),
+                 Queen(isWhite: false),
+                 King(isWhite: false)]
 
-game.start()
+for piece in allPieces {
+  print(piece)
+}
+
+let p1 = Pawn(isWhite: true, promoted: true, newPiece: .Queen)
+let p2 = Pawn(isWhite: true, promoted: false, newPiece: nil)
+let p3 = Pawn(isWhite: false, promoted: false, newPiece: nil)
+let p4 = Pawn(isWhite: false, promoted: true, newPiece: .Queen)
+let p5 = Pawn(isWhite: true, promoted: true, newPiece: .Knight)
+
+print(p1 == p2)
+print(p1 == p4)
+print(p1 == p5)
+print(p2 == p3)
+print(p4 == p5)
